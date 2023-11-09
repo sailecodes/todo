c66ed5051af30185900a8f0db47c73bf8e506e20
saile <eliasiv.codes@gmail.com>
2023-11-08 20:42:57 -0800

    add new user routes for additional functionality

--- server/controllers/userController.js
+++ server/controllers/userController.js
@@ -1,5 +1,7 @@
 import { StatusCodes } from "http-status-codes";
+
 import userModel from "../models/userModel.js";
+import todoModel from "../models/todoModel.js";
 import { NotFoundError } from "../custom-errors/customErrors.js";
 
 export const getUser = async (req, res) => {
@@ -9,3 +11,46 @@ export const getUser = async (req, res) => {
 
   res.status(StatusCodes.OK).json({ msg: "Retrieved user information.", data: user });
 };
+
+export const getUserTodosFinished = async (req, res) => {
+  const todosFinished = await todoModel.find({ createdBy: req.userInfo.userId, progress: "finished" });
+
+  res
+    .status(StatusCodes.OK)
+    .json({ msg: "Retrieved finished todos.", data: todosFinished, count: todosFinished.length });
+};
+
+export const getUserTodosPastDeadline = async (req, res) => {
+  const date = new Date();
+  const todayUnformatted = {
+    year: date.getFullYear(),
+    month: date.getMonth() + 1,
+    day: date.getDate(),
+    hour: date.getHours(),
+    minute: date.getMinutes(),
+  };
+  const todayFormatted = `${todayUnformatted.month < 10 ? `0` + todayUnformatted.month : todayUnformatted.month}/${
+    todayUnformatted.day < 10 ? `0` + todayUnformatted.day : todayUnformatted.day
+  }/${todayUnformatted.year} ${todayUnformatted.hour < 10 ? `0` + todayUnformatted.hour : todayUnformatted.hour}:${
+    todayUnformatted.minute < 10 ? `0` + todayUnformatted.minute : todayUnformatted.minute
+  }`;
+
+  const todosPastDeadline = await todoModel.find({
+    createdBy: req.userInfo.userId,
+    progress: { $in: ["just started", "halfway there"] },
+    deadline: { $gte: todayFormatted },
+  });
+
+  res
+    .status(StatusCodes.OK)
+    .json({ msg: "Retrieved todos past deadline.", data: todosPastDeadline, count: todosPastDeadline.length });
+};
+
+export const getUserTodoDueMostSoon = async (req, res) => {
+  const todosDueMostSoon = await todoModel.find({
+    createdBy: req.userInfo.userId,
+    progress: { $in: ["just started", "halfway there"] },
+  });
+};
+
+export const getUserTodosComing = async (req, res) => {};
--- server/routers/userRouter.js
+++ server/routers/userRouter.js
@@ -1,8 +1,18 @@
 import { Router } from "express";
-import { getUser } from "../controllers/userController.js";
+import {
+  getUser,
+  getUserTodosFinished,
+  getUserTodosPastDeadline,
+  getUserTodoDueMostSoon,
+  getUserTodosComing,
+} from "../controllers/userController.js";
 
 const userRouter = Router();
 
-userRouter.get("/user", getUser);
+userRouter.get("/", getUser);
+userRouter.get("/todos-finished", getUserTodosFinished);
+userRouter.get("/todos-past-deadline", getUserTodosPastDeadline);
+userRouter.get("/todos-due-most-soon", getUserTodoDueMostSoon);
+userRouter.get("/todos-coming", getUserTodosComing);
 
 export default userRouter;
